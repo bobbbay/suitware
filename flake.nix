cee5b6650976537b00e89318a0752580ee34cb05
bobbbay <abatterysingle@gmail.com>
2021-11-29 10:27:41 -0500

    [general] Create heads-up-display project

--- flake.nix
+++ flake.nix
@@ -2,5 +2,34 @@
   inputs = {
     cargo.url = "github:yusdacra/nix-cargo-integration";
   };
-  outputs = inputs: inputs.cargo.lib.makeOutputs { root = ./.; };
+  outputs = inputs:
+  let
+    shellInputs = pkgs: with pkgs; [
+    ];
+    appNativeBuildInputs = pkgs: with pkgs; [
+      pkg-config
+    ];
+    appBuildInputs = pkgs: (appRuntimeInputs pkgs) ++ (with pkgs; [
+      udev alsaLib x11
+      vulkan-tools vulkan-headers vulkan-validation-layers
+    ]);
+    appRuntimeInputs = pkgs: with pkgs; [
+      vulkan-loader
+      xlibs.libXcursor xlibs.libXi xlibs.libXrandr
+    ];
+  in inputs.cargo.lib.makeOutputs {
+    root = ./.;
+
+    overrides = {
+      common = prev: {
+        runtimeLibs = prev.runtimeLibs ++ (appRuntimeInputs prev.pkgs);
+        buildInputs = prev.buildInputs ++ (appBuildInputs prev.pkgs);
+        nativeBuildInputs = prev.nativeBuildInputs ++ (appNativeBuildInputs prev.pkgs);
+      };
+
+      shell = common: prev: {
+        packages = prev.packages ++ (shellInputs common.pkgs);
+      };
+    };
+  };
 }
