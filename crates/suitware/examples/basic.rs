39794e058a389a312f2ff736af39f54fac296406
bobbbay <abatterysingle@gmail.com>
2021-12-31 16:44:51 -0500

    chore: format all files

--- crates/suitware/examples/basic.rs
+++ crates/suitware/examples/basic.rs
@@ -28,9 +28,9 @@ impl TemperatureSensor {
     /// Create a new TemperatureSensor, with default values.
     fn new() -> Self {
         Self {
-	    // Here, we create a handle to the mqttclient plugin, and store it
-	    // locally.
-	    mqtt: MqttClient::new("some-id", "localhost", 5001, 100),
+            // Here, we create a handle to the mqttclient plugin, and store it
+            // locally.
+            mqtt: MqttClient::new("some-id", "localhost", 5001, 100),
             rng: rand_pcg::Pcg32::seed_from_u64(13),
         }
     }
@@ -43,28 +43,34 @@ impl System for TemperatureSensor {
     /// loop. It can determine the next state of itself by return a variant of
     /// [`NextState`].
     async fn run(&mut self) -> Result<NextState, &dyn std::error::Error> {
-	// Use the included rng to generate a random f32.
+        // Use the included rng to generate a random f32.
         let random_value: f32 = self.rng.gen::<f32>() * 20.0;
 
-	// Serialize it with bincode, so we can send it over Mqtt.
+        // Serialize it with bincode, so we can send it over Mqtt.
         let serialized = bincode::serialize(&random_value).unwrap();
 
-	// Publish!
-	// Note that since this is run from inside a Tokio runtime, we can use
-	// `.await` syntax to start futures.
-        self.mqtt.client
-            .publish("temperature_sensor/get", QoS::AtLeastOnce, false, serialized)
+        // Publish!
+        // Note that since this is run from inside a Tokio runtime, we can use
+        // `.await` syntax to start futures.
+        self.mqtt
+            .client
+            .publish(
+                "temperature_sensor/get",
+                QoS::AtLeastOnce,
+                false,
+                serialized,
+            )
             .await
             .unwrap();
 
-	// We need to poll the eventloop for `rumqttc` to make sure it sends the
-	// message.
+        // We need to poll the eventloop for `rumqttc` to make sure it sends the
+        // message.
         self.mqtt.eventloop.poll().await.unwrap();
 
-	// Let's wait a bit for good measure.
+        // Let's wait a bit for good measure.
         tokio::time::sleep(Duration::from_secs(1)).await;
 
-	// Let's ask the runner to loop again next time.
+        // Let's ask the runner to loop again next time.
         Ok(NextState::Continue)
     }
 }
